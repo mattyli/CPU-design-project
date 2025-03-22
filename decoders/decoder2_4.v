3235e594cd2541f2e4e4549d15ffedfce0705844
Matthew Li <matthew.li@queensu.ca>
2025-03-22 18:38:04 -0400

    beginning to add CON_FF logic and decoder logic required for CON FF

---
+++ decoders/decoder2_4.v
@@ -0,0 +1,14 @@
+module decoder2_4 (
+    input [1:0] in,
+    output reg [3:0] out
+)
+    always @(*) begin
+        out = 4'd0;
+        case (in)
+            2'b00: out[0] = 1;
+            2'b01: out[1] = 1;
+            2'b10: out[2] = 1;
+            2'b11: out[3] = 1;
+        endcase 
+    end
+endmodule
